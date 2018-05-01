9ebdd0dcffeba5d58973edbcc3190a581a7d1782
barac john <barakcodes@gmail.com>
2018-05-01 18:11:57 +0300

    ::smiley

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,19 @@
 import { StackNavigator } from 'react-navigation'
+import View from '../Containers/View'
 import LaunchScreen from '../Containers/LaunchScreen'
+import ListView from '../Containers/View';
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  View: { screen: View },
+  ListView: { screen: ListView },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'ListView',
   navigationOptions: {
     headerStyle: styles.header
   }
